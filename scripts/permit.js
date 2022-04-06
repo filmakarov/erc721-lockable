283aee807c69f215d8fcc587c96804f554df914a
Nikita <nakit21@gmail.com>
2022-04-06 19:35:23 +0300

    fix names

--- scripts/permit.js
+++ scripts/permit.js
@@ -10,55 +10,55 @@ const nonce = 0;
 const spender = process.env.SPENDER;
 
   
-    const typedData = JSON.stringify({
-      types: {
-        EIP712Domain: [
-          {
-            name: "name",
-            type: "string"
-          },
-          {
-            name: "version",
-            type: "string"
-          },
-          {
-            name: "chainId",
-            type: "uint256"
-          },
-          {
-            name: "verifyingContract",
-            type: "address"
-          }
-        ],
-        Permit: [
-            { name: 'signer', type: 'address' },
-            { name: 'spender', type: 'address' },
-            { name: 'nonce', type: 'uint256' },
-            { name: 'deadline', type: 'uint256' }
-        ]
+const typedData = JSON.stringify({
+  types: {
+    EIP712Domain: [
+      {
+        name: "name",
+        type: "string"
       },
-      primaryType: "Permit",
-      domain: {
-        name: "MockNFT",
-        version: '1',
-        chainId: "4",
-        verifyingContract: "0x356D9A6a699B5C2F080FFbA653683D1efF5a79af"
+      {
+        name: "version",
+        type: "string"
       },
-      message: {
-        signer: fromAddress,
-        spender: spender,
-        nonce: nonce,
-        deadline: expiry
+      {
+        name: "chainId",
+        type: "uint256"
+      },
+      {
+        name: "verifyingContract",
+        type: "address"
       }
-    });
-  //web3.eth.signTypedData(typedData, process.env.WALLET);
-  web3.currentProvider.send({
-    method: "eth_signTypedData_v4",
-    params: [fromAddress, typedData],
-    from: fromAddress
+    ],
+    Permit: [
+        { name: 'signer', type: 'address' },
+        { name: 'spender', type: 'address' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'deadline', type: 'uint256' }
+    ]
   },
-  function (err, result) {
-    if (err) return console.dir(err);
-    if (result.error) return console.error('ERROR', result);
-    console.log('TYPED SIGNED:' + JSON.stringify(result.result));
-  });
+  primaryType: "Permit",
+  domain: {
+    name: "MockNFT",
+    version: '1',
+    chainId: "4",
+    verifyingContract: "0x356D9A6a699B5C2F080FFbA653683D1efF5a79af"
+  },
+  message: {
+    signer: fromAddress,
+    spender: spender,
+    nonce: nonce,
+    deadline: expiry
+  }
+});
+//web3.eth.signTypedData(typedData, process.env.WALLET);
+web3.currentProvider.send({
+  method: "eth_signTypedData_v4",
+  params: [fromAddress, typedData],
+  from: fromAddress
+},
+function (err, result) {
+  if (err) return console.dir(err);
+  if (result.error) return console.error('ERROR', result);
+  console.log('TYPED SIGNED:' + JSON.stringify(result.result));
+});
