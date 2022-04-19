7529c9af1ead992ee238d5a998b46ae26882c9e6
Nikita <nakit21@gmail.com>
2022-04-20 01:25:33 +0300

    add rent function

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import "./ERC721s.sol";
+import "./MockNFT.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTMarketplace {
+contract NFTMarketplace is Ownable {
     bytes4 private constant FUNC_SELECTOR = bytes4(keccak256("getLocked(uint256)"));
+
+    address payable public wallet; 
 
     struct OfferData {
         uint256 minTime;
@@ -12,6 +15,7 @@ contract NFTMarketplace {
         uint256 startDiscountTime;
         uint256 tokenId;
         uint256 price;
+        uint256 endTime;
         address _token;
     }
 
@@ -27,8 +31,8 @@ contract NFTMarketplace {
     returns(bool)
     {   
         require(checkLock(_token, tokenId), "token is locked");
-        ERC721s(_token).transferFrom(msg.sender, address(this), tokenId);
-        userOffers[msg.sender].push(OfferData({tokenId: tokenId, minTime: minTime, maxTime: maxTime, startDiscountTime: startDiscountTime, price: price, _token: _token}));
+        MockNFT(_token).transferFrom(msg.sender, address(this), tokenId);
+        userOffers[msg.sender].push(OfferData({tokenId: tokenId, minTime: minTime, maxTime: maxTime, startDiscountTime: startDiscountTime, price: price, endTime: 0, _token: _token}));
 
         return true;
     }
@@ -41,23 +45,35 @@ contract NFTMarketplace {
         for(uint i = 0; i < tokenIds.length; i++)
         {
             require(checkLock(_token, tokenIds[i]), "token is locked");
-            ERC721s(_token).transferFrom(msg.sender, address(this), tokenIds[i]);
-            userOffers[msg.sender].push(OfferData({tokenId: tokenIds[i], minTime: minTimes[i], maxTime: maxTimes[i], startDiscountTime: startDiscountTimes[i], price: prices[i], _token: _token}));
+            MockNFT(_token).transferFrom(msg.sender, address(this), tokenIds[i]);
+            userOffers[msg.sender].push(OfferData({tokenId: tokenIds[i], minTime: minTimes[i], maxTime: maxTimes[i], startDiscountTime: startDiscountTimes[i], price: prices[i], endTime: 0, _token: _token}));
         }
         return true;
     }
 
-    function offerAll(address _token, uint256[] calldata tokenIds, uint256 minTime, uint256 maxTime, uint256 startDiscountTime, uint256 price)
-    public 
+    function rent(address _token, address landlord, uint256 tokenId, uint256 rentTime) 
+    public
     payable
     returns(bool)
-    {   
-        for(uint i = 0; i < tokenIds.length; i++)
-        {
-            require(checkLock(_token, tokenIds[i]), "token is locked");
-            ERC721s(_token).transferFrom(msg.sender, address(this), tokenIds[i]);
-            userOffers[msg.sender].push(OfferData({tokenId: tokenIds[i], minTime: minTime, maxTime: maxTime, startDiscountTime: startDiscountTime, price: price, _token: _token}));
+    {
+        uint numOffer = userOffers[landlord].length;
+
+        for(uint i = 0; i < userOffers[landlord].length - 1; i++) {
+            if (userOffers[landlord][i].tokenId == tokenId) {
+                numOffer = i;
+                break;
+            }
         }
+
+        require(numOffer < userOffers[landlord].length, "");
+        require( userOffers[landlord][numOffer].price == msg.value, "");
+        require(rentTime >=  userOffers[landlord][numOffer].minTime && rentTime <=  userOffers[landlord][numOffer].maxTime, "");
+
+        wallet.transfer(msg.value);
+
+        MockNFT(_token).lock(address(this), tokenId);
+        MockNFT(_token).transferFrom(address(this), msg.sender, tokenId);
+        userOffers[landlord][numOffer].endTime = rentTime + block.timestamp;
 
         return true;
     }
@@ -89,5 +105,15 @@ contract NFTMarketplace {
         require(isLockingContract(_token), "contract does not support locking");
         address locker = ERC721s(_token).getLocked(tokenId);
         return locker == address(0) ? true : false;
+    }
+
+    function _setWallet(address payable _wallet)
+        external
+        onlyOwner
+        returns (bool) 
+    {
+        wallet = _wallet;
+
+        return true;
     }
 }
